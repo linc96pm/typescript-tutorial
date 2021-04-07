589d203ac1316532b709560c15e3c852622e9f67
Mukhammad Pardaboev <mukhammad.lb.96@gmail.com>
2021-04-07 15:28:33 +0500

    eight lesson

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,3 +1,16 @@
 "use strict";
-// lesson-7
-console.log('test');
+// lesson-8 Function types
+var greet;
+greet = function () {
+    console.log("Hello world");
+};
+var add = function (a, b, c) {
+    if (c === void 0) { c = 10; }
+    console.log(a + b);
+    console.log(c);
+};
+add(1, 10, '20');
+var minus = function (a, b) {
+    return a + b;
+};
+var result = minus(5, 10);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,2 +1,21 @@
-// lesson-7
-console.log('test')
+// lesson-8 Function types
+
+let greet: Function;
+
+greet = () => {
+    console.log("Hello world");
+}
+
+
+const add = (a: number, b: number, c: number | string = 10) => {
+    console.log(a + b);
+    console.log(c);
+};
+
+add(1, 10, '20');
+
+const minus = (a: number, b: number) => {
+    return a + b;
+}
+
+let result = minus(5, 10);
